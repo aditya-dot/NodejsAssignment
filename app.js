e9406fe24e022b1bd65f2414da853c40be4c3189
patiladitya <patiladityarao@gmail.com>
2019-10-19 12:38:16 +0530

    first version

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express')
+bodyParser = require('body-parser')
+var app = express()
+
+app.listen(3000)
+
+
+// app.post('/hello', function (req, res) {
+//     res.send("You just called the post method at '/hello'!\n");
+// });
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.use(function (req, res, next) {
+    console.log("A new request receive" + new Date());
+    next();
+})
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+
